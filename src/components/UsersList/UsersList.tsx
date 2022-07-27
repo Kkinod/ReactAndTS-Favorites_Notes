377a87f7dbe64dc1312e1118271a34efc32e949b
Kamil <naszewlasnepl@gmail.com>
2022-07-27 20:41:08 +0200

    add component: users list

---
+++ src/components/UsersList/UsersList.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const UsersList = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <div>2.3</div>
+          <div>
+            <p>Kamil Pawełek</p>
+            <p>attendance: 38%</p>
+          </div>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default UsersList
---
+++ src/data/users.tsx
@@ -0,0 +1,22 @@
+export const users = [
+  {
+    name: 'Kamil Kamilczak',
+    attendance: '45%',
+    average: '4.3',
+  },
+  {
+    name: 'Agata Filipowska',
+    attendance: '35%',
+    average: '5.3',
+  },
+  {
+    name: 'Robert Pawelek',
+    attendance: '40%',
+    average: '4.0',
+  },
+  {
+    name: 'Krzysztof Pawelek',
+    attendance: '45%',
+    average: '3.3',
+  },
+]
