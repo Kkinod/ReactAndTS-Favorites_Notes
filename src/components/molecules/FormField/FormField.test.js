8c0edecf257d4404c87421f64dbebec57bf74205
Kamil <naszewlasnepl@gmail.com>
2022-08-08 22:07:17 +0200

    add first tests: unit and integrative

---
+++ src/components/molecules/FormField/FormField.test.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import { fireEvent, render, screen } from '@testing-library/react'
+import FormField from './FormField'
+import { renderWithProviders } from '../../../helpers/renderWithThemeProviders'
+
+describe('Form Field', () => {
+    it('Renders the component', () => {
+        renderWithProviders(<FormField label='name' name='name' id='id' />)
+    })
+})
+
+// Fake input and button
+const InputWithButton = () => {
+    const [inputValue, setInputValue] = useState('')
+
+    const handleInputChange = (e) => setInputValue(e.target.value)
+
+    return (
+        <>
+            <input
+                name='Name'
+                id='name'
+                value={inputValue}
+                onChange={handleInputChange}
+                placeholder='Enter your name'
+            />
+            <button disabled={!inputValue}>Submit</button>
+        </>
+    )
+}
+
+describe('Input With Button', () => {
+    it('Renders the component', () => {
+        render(<InputWithButton />)
+        screen.getByText('Submit')
+    })
+
+    it('Properly handles value change', () => {
+        render(<InputWithButton />)
+        const input = screen.getByPlaceholderText('Enter your name')
+        fireEvent.change(input, { target: { value: 'Kamil' } })
+        expect(input).toHaveValue('Kamil')
+    })
+
+    it('Checking if the button is disabled/enabled when the input is empty/have value', () => {
+        render(<InputWithButton />)
+        const button = screen.getByText('Submit')
+        expect(button).toBeDisabled()
+
+        const input = screen.getByPlaceholderText('Enter your name')
+        fireEvent.change(input, { target: { value: 'Kamil' } })
+        expect(button).not.toBeDisabled()
+    })
+})
