7b8c0860443f5a015bbedc86fc5b24927a5005e3
Kamil <naszewlasnepl@gmail.com>
2022-09-07 16:50:27 +0200

    add mocking api

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './views/App'
+import { worker } from './mocks/browser'
+
+worker.start()
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
--- src/providers/UsersProviders.tsx
+++ src/providers/UsersProviders.tsx
@@ -1,23 +1,24 @@
-import React, { useState } from 'react'
-import { users as usersData } from '../data/users'
-
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 export interface IChildren {
     children?: React.ReactNode
 }
 
 export interface IUsersList {
+    id?: string
     name: string
     attendance: string
     average: string
+    group?: string
 }
 
-interface Context {
+interface IContext {
     users: IUsersList[]
     handleAddWorker: (p: IUsersList) => void
     deleteWorker: (p: string) => void
 }
 
-export const UsersContext = React.createContext<Context>({
+export const UsersContext = React.createContext<IContext>({
     users: [],
     // eslint-disable-next-line @typescript-eslint/no-empty-function
     handleAddWorker: () => {},
@@ -26,7 +27,14 @@ export const UsersContext = React.createContext<Context>({
 })
 
 const UsersProvider = ({ children }: IChildren) => {
-    const [users, setUsers] = useState(usersData)
+    const [users, setUsers] = useState<IUsersList[]>([])
+
+    useEffect(() => {
+        axios
+            .get('/students')
+            .then(({ data }) => setUsers(data.students))
+            .catch((err) => console.log(err))
+    }, [])
 
     const deleteWorker = (name: string) => {
         const filteredUsers = users.filter((user) => user.name !== name)
