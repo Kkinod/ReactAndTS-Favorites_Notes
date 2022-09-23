a59bc61e155a8903720a9bf648cefd2d8b20779f
Kamil <naszewlasnepl@gmail.com>
2022-09-23 10:35:17 +0200

    adding a test for login

--- cypress/e2e/cypress/e2e/spec.cy.ts
+++ cypress/e2e/cypress/e2e/spec.cy.ts
@@ -1,5 +1,18 @@
 describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:3000/')
-  })
+    it('Renders unauthenticated app', () => {
+        cy.visit('http://localhost:3000/')
+        cy.findByText(/login/i).should('exist')
+    })
+
+    it('Allows user to authenticate', () => {
+        cy.visit('http://localhost:3000/')
+        cy.findByText(/login/i).should('exist')
+        cy.findByText(/login/i).click().type('teacher@studybuddy.com')
+        cy.findByText(/password/i)
+            .click()
+            .type('Test123')
+        cy.findByText(/sign in/i).click()
+
+        cy.findByText(/logged as/i).should('exist')
+    })
 })
